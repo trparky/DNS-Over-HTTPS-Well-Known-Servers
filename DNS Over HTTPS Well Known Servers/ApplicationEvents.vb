714d619adf9b5c6ca76f8383b34a0815e1e50483
Tom Parkison <trparky@toms-world.org>
2020-12-17 11:08:54 -0500

    Added an indication at program startup that only versions of Windows in the Windows Insider Program are currently supported. This code will change when the version of Windows 10 that natively supports DoH is released publically.

--- DNS Over HTTPS Well Known Servers/ApplicationEvents.vb
+++ DNS Over HTTPS Well Known Servers/ApplicationEvents.vb
@@ -9,6 +9,13 @@ Namespace My
     ' NetworkAvailabilityChanged: Raised when the network connection is connected or disconnected.
     Partial Friend Class MyApplication
         Private Sub MyApplication_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
+            If Environment.OSVersion.Version.Build <= 19042 Then
+                ' This message will change when the version of Windows 10 that natively supports DoH is released publically.
+                MsgBox("This version of Windows 10 is not supported. Currently only versions in the Windows Insider Program are supported." & vbCrLf & vbCrLf & "This program will now close.", MsgBoxStyle.Information, "DNS Over HTTPS Well Known Servers")
+                e.Cancel = True
+                Exit Sub
+            End If
+
             If Application.CommandLineArgs.Count = 1 Then
                 Dim commandLineArgument As String = Application.CommandLineArgs(0).Trim
                 If commandLineArgument.Equals("-update", StringComparison.OrdinalIgnoreCase) Then DoUpdateAtStartup()
