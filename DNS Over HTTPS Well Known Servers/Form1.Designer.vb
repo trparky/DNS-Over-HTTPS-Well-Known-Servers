73037a8b9fb0fa79c4af172e96c770520e071f42
Tom Parkison <trparky@toms-world.org>
2020-12-17 10:31:04 -0500

    Add project files.

---
+++ DNS Over HTTPS Well Known Servers/Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListServers = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.BtnAddServer = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TxtIPAddress = New System.Windows.Forms.TextBox()
+        Me.TxtURL = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TxtDeviceName = New System.Windows.Forms.TextBox()
+        Me.BtnDelete = New System.Windows.Forms.Button()
+        Me.BtnEdit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(10, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(194, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "DNS Over HTTPS Well Known Servers"
+        '
+        'ListServers
+        '
+        Me.ListServers.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1})
+        Me.ListServers.HideSelection = False
+        Me.ListServers.Location = New System.Drawing.Point(10, 23)
+        Me.ListServers.Name = "ListServers"
+        Me.ListServers.Size = New System.Drawing.Size(264, 332)
+        Me.ListServers.TabIndex = 2
+        Me.ListServers.UseCompatibleStateImageBehavior = False
+        Me.ListServers.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Name = "ColumnHeader1"
+        Me.ColumnHeader1.Text = "Server IP Address"
+        Me.ColumnHeader1.Width = 260
+        '
+        'BtnAddServer
+        '
+        Me.BtnAddServer.Enabled = False
+        Me.BtnAddServer.Location = New System.Drawing.Point(279, 140)
+        Me.BtnAddServer.Name = "BtnAddServer"
+        Me.BtnAddServer.Size = New System.Drawing.Size(107, 23)
+        Me.BtnAddServer.TabIndex = 3
+        Me.BtnAddServer.Text = "Add DOH Server"
+        Me.BtnAddServer.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(279, 23)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "IP Address"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(279, 62)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "URL"
+        '
+        'TxtIPAddress
+        '
+        Me.TxtIPAddress.Location = New System.Drawing.Point(279, 39)
+        Me.TxtIPAddress.Name = "TxtIPAddress"
+        Me.TxtIPAddress.Size = New System.Drawing.Size(237, 20)
+        Me.TxtIPAddress.TabIndex = 6
+        '
+        'TxtURL
+        '
+        Me.TxtURL.Location = New System.Drawing.Point(279, 77)
+        Me.TxtURL.Name = "TxtURL"
+        Me.TxtURL.Size = New System.Drawing.Size(237, 20)
+        Me.TxtURL.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(279, 100)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(120, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Device Name (Optional)"
+        '
+        'TxtDeviceName
+        '
+        Me.TxtDeviceName.Location = New System.Drawing.Point(279, 115)
+        Me.TxtDeviceName.Name = "TxtDeviceName"
+        Me.TxtDeviceName.Size = New System.Drawing.Size(236, 20)
+        Me.TxtDeviceName.TabIndex = 9
+        '
+        'BtnDelete
+        '
+        Me.BtnDelete.Enabled = False
+        Me.BtnDelete.Location = New System.Drawing.Point(10, 360)
+        Me.BtnDelete.Name = "BtnDelete"
+        Me.BtnDelete.Size = New System.Drawing.Size(130, 23)
+        Me.BtnDelete.TabIndex = 10
+        Me.BtnDelete.Text = "Delete"
+        Me.BtnDelete.UseVisualStyleBackColor = True
+        '
+        'BtnEdit
+        '
+        Me.BtnEdit.Enabled = False
+        Me.BtnEdit.Location = New System.Drawing.Point(146, 360)
+        Me.BtnEdit.Name = "BtnEdit"
+        Me.BtnEdit.Size = New System.Drawing.Size(130, 23)
+        Me.BtnEdit.TabIndex = 11
+        Me.BtnEdit.Text = "Edit"
+        Me.BtnEdit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(525, 390)
+        Me.Controls.Add(Me.BtnEdit)
+        Me.Controls.Add(Me.BtnDelete)
+        Me.Controls.Add(Me.TxtDeviceName)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TxtURL)
+        Me.Controls.Add(Me.TxtIPAddress)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.BtnAddServer)
+        Me.Controls.Add(Me.ListServers)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "DNS Over HTTPS Well Known Servers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListServers As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents BtnAddServer As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TxtIPAddress As TextBox
+    Friend WithEvents TxtURL As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TxtDeviceName As TextBox
+    Friend WithEvents BtnDelete As Button
+    Friend WithEvents BtnEdit As Button
+End Class
---
+++ DNS Over HTTPS Well Known Servers/Form1.vb
@@ -0,0 +1,121 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadServers()
+    End Sub
+
+    Private Sub LoadServers()
+        ListServers.Items.Clear()
+
+        Using RegistryKey As RegistryKey = Registry.LocalMachine.OpenSubKey("SYSTEM\CurrentControlSet\Services\Dnscache\Parameters\DohWellKnownServers", False)
+            If RegistryKey IsNot Nothing Then
+                For Each StrServerIP As String In RegistryKey.GetSubKeyNames()
+                    ListServers.Items.Add(New ListViewItem(StrServerIP))
+                Next
+            Else
+                MsgBox("Error loading known DNS Over HTTPS Well Known Servers from Registry.", MsgBoxStyle.Critical, "DNS Over HTTPS Well Known Servers")
+            End If
+        End Using
+    End Sub
+
+    Private Sub BtnAddServer_Click(sender As Object, e As EventArgs) Handles BtnAddServer.Click
+        Try
+            Dim StrURL As String = TxtURL.Text
+            If Not String.IsNullOrWhiteSpace(TxtDeviceName.Text) Then StrURL &= "/" & HttpUtility.UrlEncode(TxtDeviceName.Text)
+
+            If BtnAddServer.Text.Equals("Add DOH Server", StringComparison.OrdinalIgnoreCase) Then
+                Using RegistryKey As RegistryKey = Registry.LocalMachine.OpenSubKey("SYSTEM\CurrentControlSet\Services\Dnscache\Parameters\DohWellKnownServers", True)
+                    RegistryKey.CreateSubKey(TxtIPAddress.Text)
+                End Using
+            End If
+
+            Using RegistryKey As RegistryKey = Registry.LocalMachine.OpenSubKey("SYSTEM\CurrentControlSet\Services\Dnscache\Parameters\DohWellKnownServers\" & TxtIPAddress.Text, True)
+                RegistryKey.SetValue("Template", StrURL, RegistryValueKind.String)
+                RegistryKey.SetValue("DeviceID", TxtDeviceName.Text, RegistryValueKind.String)
+                RegistryKey.SetValue("URL", TxtURL.Text, RegistryValueKind.String)
+            End Using
+
+            If BtnAddServer.Text.Equals("Add DOH Server", StringComparison.OrdinalIgnoreCase) Then
+                MsgBox("DNS Over HTTPS Server added successfully.", MsgBoxStyle.Information, "DNS Over HTTPS Well Known Servers")
+            Else
+                TxtIPAddress.Enabled = True
+                MsgBox("DNS Over HTTPS Server edited successfully.", MsgBoxStyle.Information, "DNS Over HTTPS Well Known Servers")
+            End If
+
+            TxtDeviceName.Text = Nothing
+            TxtIPAddress.Text = Nothing
+            TxtURL.Text = Nothing
+            BtnAddServer.Text = "Add DOH Server"
+
+            LoadServers()
+        Catch ex As Exception
+            MsgBox("There was an error adding the DNS Over HTTPS server. Make sure you have Administrative access.", MsgBoxStyle.Critical, "DNS Over HTTPS Well Known Servers")
+        End Try
+    End Sub
+
+    Private Sub BtnDelete_Click(sender As Object, e As EventArgs) Handles BtnDelete.Click
+        Try
+            Using RegistryKey As RegistryKey = Registry.LocalMachine.OpenSubKey("SYSTEM\CurrentControlSet\Services\Dnscache\Parameters\DohWellKnownServers", True)
+                RegistryKey.DeleteSubKeyTree(ListServers.SelectedItems(0).Text)
+            End Using
+        Catch ex As Exception
+            MsgBox("There was an error deleting the DNS Over HTTPS server. Make sure you have Administrative access.", MsgBoxStyle.Critical, "DNS Over HTTPS Well Known Servers")
+        End Try
+    End Sub
+
+    Private Sub TxtIPAddress_TextChanged(sender As Object, e As EventArgs) Handles TxtIPAddress.TextChanged
+        ActivateAddServerButton()
+    End Sub
+
+    Private Sub TxtURL_TextChanged(sender As Object, e As EventArgs) Handles TxtURL.TextChanged
+        ActivateAddServerButton()
+    End Sub
+
+    Private Sub TxtDeviceName_TextChanged(sender As Object, e As EventArgs) Handles TxtDeviceName.TextChanged
+        ActivateAddServerButton()
+    End Sub
+
+    Private Sub ActivateAddServerButton()
+        BtnAddServer.Enabled = Not String.IsNullOrWhiteSpace(TxtDeviceName.Text) And Not String.IsNullOrWhiteSpace(TxtURL.Text) And Not String.IsNullOrWhiteSpace(TxtDeviceName.Text)
+    End Sub
+
+    Private Sub ListServers_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListServers.SelectedIndexChanged
+        If ListServers.SelectedItems.Count = 0 Then
+            BtnDelete.Enabled = False
+            BtnEdit.Enabled = False
+        Else
+            BtnDelete.Enabled = True
+            BtnEdit.Enabled = True
+        End If
+    End Sub
+
+    Private Sub BtnEdit_Click(sender As Object, e As EventArgs) Handles BtnEdit.Click
+        TxtURL.Text = Nothing
+        TxtDeviceName.Text = Nothing
+        TxtIPAddress.Text = Nothing
+
+        Try
+            Using RegistryKey As RegistryKey = Registry.LocalMachine.OpenSubKey("SYSTEM\CurrentControlSet\Services\Dnscache\Parameters\DohWellKnownServers\" & ListServers.SelectedItems(0).Text, True)
+                Dim StrTemplate As String = RegistryKey.GetValue("Template", Nothing)
+
+                If Not String.IsNullOrWhiteSpace(StrTemplate) Then
+                    Dim StrURL As String = RegistryKey.GetValue("URL", Nothing)
+                    Dim StrDeviceID As String = RegistryKey.GetValue("DeviceID", Nothing)
+
+                    TxtIPAddress.Text = ListServers.SelectedItems(0).Text
+                    TxtIPAddress.Enabled = False
+
+                    If String.IsNullOrWhiteSpace(StrURL) Or String.IsNullOrWhiteSpace(StrDeviceID) Then
+                        TxtURL.Text = HttpUtility.UrlDecode(StrTemplate)
+                    Else
+                        TxtURL.Text = StrURL
+                        TxtDeviceName.Text = StrDeviceID
+                    End If
+
+                    BtnAddServer.Text = "Edit DOH Server"
+                End If
+            End Using
+        Catch ex As Exception
+            MsgBox("There was an error editing the DNS Over HTTPS server. Make sure you have Administrative access.", MsgBoxStyle.Critical, "DNS Over HTTPS Well Known Servers")
+        End Try
+    End Sub
+End Class
