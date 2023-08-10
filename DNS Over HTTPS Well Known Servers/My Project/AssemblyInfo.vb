14ef95d0cab9b436265527fa8322e9b42eefadf8
Thomas Parkison <trparky@toms-world.org>
2023-08-10 14:54:04 -0400

    Updated copyright in AssemblyInfo.vb.

--- DNS Over HTTPS Well Known Servers/My Project/AssemblyInfo.vb
+++ DNS Over HTTPS Well Known Servers/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("DNS Over HTTPS Well Known Servers")>
-<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyCopyright("Copyright Thomas Parkison © 2023-2024")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
