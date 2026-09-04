c6e20a658ab813a1dbeed2cf08a7213b04c79522
Thomas Parkison <trparky@toms-world.org>
2026-09-03 21:19:11 -0400

    Fix version parsing failing on systems with non-US decimal separator (comma) culture settings.

--- DNS Over HTTPS Well Known Servers/Check for Update Stuff.vb
+++ DNS Over HTTPS Well Known Servers/Check for Update Stuff.vb
@@ -9,12 +9,11 @@ Namespace checkForUpdates
         Public Const strProgramName As String = "DNS Over HTTPS Well Known Servers"
         ' Change these variables whenever you import this module into a program's code to handle software updates.
 
-        Public versionString As String
-        Public versionInfo As String() = Application.ProductVersion.Split(".")
+        Public ReadOnly versionInfo As New Version(Application.ProductVersion)
+        Public ReadOnly versionString As String = $"{versionInfo.Major}.{versionInfo.Minor} Build {versionInfo.Build}"
 
         Sub New()
-            versionString = $"{versionInfo(VersionPieces.major)}.{versionInfo(VersionPieces.minor)} Build {versionInfo(VersionPieces.build)}"
-            If IsDebugBuild() And Integer.Parse(versionInfo(VersionPieces.revision)) <> 0 Then versionString &= $" (Debug Build {versionInfo(VersionPieces.revision)})"
+            If IsDebugBuild() And Integer.Parse(versionInfo.Revision) <> 0 Then versionString &= $" (Debug Build {versionInfo.Revision})"
         End Sub
 
         Private Function IsDebugBuild() As Boolean
@@ -35,9 +34,6 @@ Namespace checkForUpdates
         ' Change these variables whenever you import this module into a program's code to handle software updates.
 
         Public windowObject As Form1
-        Private ReadOnly shortBuild As Short = Short.Parse(versionInfo(VersionPieces.build).Trim)
-        Private ReadOnly versionStringWithoutBuild As Double = Double.Parse($"{versionInfo(VersionPieces.major)}.{versionInfo(VersionPieces.minor)}")
-        Private ReadOnly longInternalVersion As Long = Long.Parse(versionInfo(VersionPieces.revision))
 
         Public Sub New(inputWindowObject As Form1)
             windowObject = inputWindowObject
@@ -80,11 +76,11 @@ Namespace checkForUpdates
                 Dim longInternalVersionFromXML As Long = 0
                 If xmlNode.SelectSingleNode("internalversion") IsNot Nothing Then
                     If Long.TryParse(xmlNode.SelectSingleNode("internalversion").InnerText.Trim, longInternalVersionFromXML) Then
-                        If longInternalVersionFromXML = longInternalVersion Then ' If the internal version from the XML file matches the internal version from the program itself, we return a noUpdateNeeded value.
+                        If longInternalVersionFromXML = versionInfo.Revision Then ' If the internal version from the XML file matches the internal version from the program itself, we return a noUpdateNeeded value.
                             Return ProcessUpdateXMLResponse.noUpdateNeeded
-                        ElseIf longInternalVersionFromXML > longInternalVersion Then ' If the internal version from the XML file is greater than the internal version from the program itself, we return a newVersion value.
+                        ElseIf longInternalVersionFromXML > versionInfo.Revision Then ' If the internal version from the XML file is greater than the internal version from the program itself, we return a newVersion value.
                             Return ProcessUpdateXMLResponse.newVersion
-                        ElseIf longInternalVersionFromXML < longInternalVersion Then
+                        ElseIf longInternalVersionFromXML < versionInfo.Revision Then
                             Return ProcessUpdateXMLResponse.newerVersionThanWebSite ' If the internal version from the XML file is less than the internal version from the program itself, we return a newerVersionThanWebSite value.
                         End If
                     Else
@@ -268,7 +264,7 @@ Namespace checkForUpdates
                     strOSName = "Windows 8"
                 ElseIf intOSMajorVersion = 6 And intOSMinorVersion = 3 Then
                     strOSName = "Windows 8.1"
-				ElseIf intOSMajorVersion = 10 Then
+                ElseIf intOSMajorVersion = 10 Then
                     strOSName = $"Windows {If(osVersion.Build >= 22000, "11", "10")}"
                 Else
                     strOSName = $"Windows NT {intOSMajorVersion}.{intOSMinorVersion}"
